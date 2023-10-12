cdcfaae0e8acd00669b571877f7b7445544858ef
Livanhernandez <livanhernandez9@gmail.com>
2023-10-12 13:41:05 -0400

    Size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -12,11 +12,11 @@ int main(void)
 	long long int lolointSize;
 	float floatSize;
 
-	printf("Size of char: %zu bytes(s)\n", sizeof(charSize));
-	printf("Size of int: %zu byte(s)\n", sizeof(intSize));
-	printf("Size of long int: %zu byte(s)\n", sizeof(lngintSize));
-	printf("Size of long long int: %zu byte(s)\n", sizeof(lolointSize));
-	printf("Size of float: %zu byte(s)\n", sizeof(floatSize));
+	printf("Size of a char: %zu bytes(s)\n", sizeof(charSize));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intSize));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(lngintSize));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(lolointSize));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatSize));
 
 	return (0);
 }
