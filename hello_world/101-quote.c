c4db2c19494e64d4bf1e462dd71d7356915f6a5e
Livanhernandez <livanhernandez9@gmail.com>
2023-10-12 14:58:19 -0400

    STDERR_FILENO

---
+++ hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(STDERR_FILENO, msg, sizeof(msg));
+	return (1);
+}
