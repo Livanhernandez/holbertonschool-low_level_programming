facd93a96feb43fe1bc1d4f2df2293a0c2106f4f
Livanhernandez <livanhernandez9@gmail.com>
2023-10-17 10:23:43 -0400

    Print alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char abc;
+
+	for (abc = 'a'; abc <= 'z'; abc++)
+		_putchar (abc);
+
+	_putchar ('\n');
+}
