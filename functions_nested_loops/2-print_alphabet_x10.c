ccfad978eb6083be5f9e1e8ab6abbe2f7a27a5ec
Livanhernandez <livanhernandez9@gmail.com>
2023-10-17 10:54:25 -0400

    alphabetx10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabet 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	int times = 10;
+	char abc;
+
+	while (times++ <= 9)
+	{
+		for (abc = 'a'; abc <= 'z'; abc++)
+			_putchar (abc);
+		_putchar ('\n');
+	}
+}
