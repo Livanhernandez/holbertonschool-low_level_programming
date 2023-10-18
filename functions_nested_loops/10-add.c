466bec3efa159a466c8466ddd2af7f1393697c14
Livanhernandez <livanhernandez9@gmail.com>
2023-10-18 01:32:34 -0400

    addition

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds two
+ * @n1: first to be added
+ * @n2: second to be added
+ *
+ * Return: result of the addition
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
