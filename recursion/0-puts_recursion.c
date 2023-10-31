1ce8bcad13e690b6e6c24f09d0f0126760071d01
Livanhernandez <livanhernandez9@gmail.com>
2023-10-31 12:26:45 -0400

    puts recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -2,17 +2,16 @@
 
 /**
  * _puts_recursion - prints a string, followed by a new line.
- * @s: The string to be printed.
+ * @s: The string.
  */
 
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
+		_putchar('\n');
+	else
 	{
 		_putchar(*s);
-		_puts_recursion(s + 1);
+		_puts_recursion(++s);
 	}
-
-	else
-		_putchar('\n');
 }
