488ec2c7c1a6289912fa8f08c55830dc93d9e4b4
Livanhernandez <livanhernandez9@gmail.com>
2023-11-03 16:00:06 -0400

    args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry poimt
+ * @argc: command line
+ * @argv: unused argument
+ * Return: Always (success)
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
