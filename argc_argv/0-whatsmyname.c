029927babe257e97e902ffae1ede9dda6b4813de
Livanhernandez <livanhernandez9@gmail.com>
2023-11-03 15:52:54 -0400

    argc

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: counts arguments
+ * @argv: s array of character pointers listing all the arguments
+ * Return: Always (success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
