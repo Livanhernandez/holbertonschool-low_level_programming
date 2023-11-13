73f0fd525c70bbb2c2b2d5fe08d6bce5676d5389
Livanhernandez <livanhernandez9@gmail.com>
2023-11-13 19:53:51 -0400

    int index

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,31 +1,29 @@
+#include <stdio.h>
 #include "function_pointers.h"
-#include <stdlib.h>
+
 /**
- * int_index - searches for an integer
- * @array: array
+ * int_index - a function that searches for an integer
+ * @array: array to pass
  * @size: number of elements in the array
- * @cmp: pointer to the function used to compare vlues
- * Return: depende of matching
+ * @cmp: pointer to the function to be used to compare values
+ *
+ * Return: the index of the first element for which the cmp function does not
+ * return 0. Otherwise, if no element matches or if size <= 0, return -1
  */
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	int c;
 
-	if (size <= 0)
+	if (size <= 0 || cmp == NULL || array == NULL)
 	{
 		return (-1);
 	}
-	if (array == NULL || cmp == NULL)
-		{
-			return (-1);
-		}
-		for (i = 0; i < size; i++)
+	for (c = 0; c < size; c++)
 	{
-		(*cmp)(array[i]);
-
-		if ((*cmp)(array[i]) != 0)
+		if ((*cmp) (array[c]))
 		{
-			return (i);
+			return (c);
 		}
 	}
 	return (-1);
