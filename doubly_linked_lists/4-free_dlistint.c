68648dc6dc19d5d02f0d5f8dff04a56986ab2608
Livanhernandez <livanhernandez9@gmail.com>
2023-12-03 17:25:54 -0400

    free list

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * free_dlistint - free doubly linked list.
+ * @head: Pointer
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
