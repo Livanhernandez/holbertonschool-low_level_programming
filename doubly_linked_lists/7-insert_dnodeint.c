738557722054156929c06d457deca1ff4edf2654
Livanhernandez <livanhernandez9@gmail.com>
2023-12-03 18:51:53 -0400

    insert list

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -8,46 +8,33 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int count = 0;
-	dlistint_t *new_node, *temp;
+	unsigned int counter = 0;
+	dlistint_t *temp, *new_node;
 
 	if (idx == 0)
-	{
 		return (add_dnodeint(h, n));
-	}
-	if (*h == NULL)
-	{
-		return (NULL);
-	}
-	temp = *h;
 
-	while (temp != NULL && count < idx -1)
+	temp = *h;
+	while ((counter < (idx - 1)) && temp)
 	{
 		temp = temp->next;
-		count++;
+		counter++;
 	}
-	if (count == idx - 1 && temp != NULL)
-	{
-		new_node = malloc(sizeof(dlistint_t));
-		if (new_node == NULL)
-		{
-			return (NULL);
-		}
 
-		new_node->n = n;
-		new_node->prev = temp;
-		new_node->next = temp->next;
-
-		if (temp->next != NULL)
-		{
-			temp->next->prev = new_node;
-		}
-		temp->next = new_node;
-
-		return (new_node);
-	}
-	else
-	{
+	if (counter < idx - 1)
 		return (NULL);
-	}
+
+	if ((counter == idx - 1) && (temp->next == NULL))
+		return (add_dnodeint_end(h, n));
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->prev = temp;
+	new_node->next = temp->next;
+	temp->next->prev = new_node;
+	temp->next = new_node;
+	return (new_node);
 }
